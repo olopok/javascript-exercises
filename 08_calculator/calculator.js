490b08a46f4a93bf6dd6df6dca9cdcf59dc17070
olopok <pabisa.pb@gmail.com>
2024-06-07 11:21:00 +0100

    Assignment done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  const sumOfAll = arr.reduce((a, b) => {
+    return a + b;
+  }, 0);
+  return sumOfAll;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  const multiplyOfAll = arr.reduce((a, b) => {
+    return a * b;
+  });
+  return multiplyOfAll;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let result = n;
+  if (n === 0 || n === 1)
+    return 1;
+  while (n > 1) {
+    n--;
+    result *= n;
+  }
+  return result;
 };
 
 // Do not edit below this line
