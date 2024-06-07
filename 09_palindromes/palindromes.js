0997fec13c6812ec2268211516dabd704780f054
olopok <pabisa.pb@gmail.com>
2024-06-07 11:22:16 +0100

    Assignment done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let arr1 = s.toLowerCase();
+    arr1 = arr1.match(/\w/ig);
+    arr1 = arr1.join('');
+    arr2 = arr1;
+    arr2 = arr2.split('').reverse().join('');
+    return arr1 === arr2;
 };
 
 // Do not edit below this line
