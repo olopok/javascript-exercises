2d5bcdb19225f7bff46e29b5b5023adc595e4026
olopok <pabisa.pb@gmail.com>
2024-06-07 15:21:00 +0100

    Assignment done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+
+    let fArr = [0, 1];
+
+    if (n <= -1) {
+        return "OOPS";
+    }
+    else {
+        for (i = 1; i <= n - 1; i++) {
+            fArr.push(fArr[i] + fArr[i - 1]);
+            console.log(fArr);
+        }
+    }
+    return fArr[n];
 
 };
 
