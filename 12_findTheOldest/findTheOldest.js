c97519baad56eca86a241321b2ecfe5ea0827be4
olopok <pabisa.pb@gmail.com>
2024-06-09 17:13:14 +0100

    12 Assignment done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(item => {
+        if (!item.yearOfDeath) {
+        item.yearOfDeath = new Date().getFullYear();}
+    });
+
+    let age =
+        people.map((person) =>
+            person.yearOfDeath - person.yearOfBirth
+        );
+    let oldPerson;
+
+    var max = age[0];
+    var maxIndex = 0;
+
+    for (i = 1; i <= age.length; i++) {
+        if (age[i] > max) {
+            maxIndex = i;
+            max = age[i];
+        }
+        oldPerson = people[maxIndex];
+    }
+    return oldPerson;
 
 };
 
